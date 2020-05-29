c628a4887508417f27e424218c667566beecab93
Maksym Polinka <Polinka7max@gmail.com>
2020-05-29 18:17:51 +0300

    added main banner navigation menue

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ document.getElementById('menue-btn').addEventListener('click', () => {
         mainTab.style.position = 'static';
     }
 });
+
+let bgCounter = 1;
+
+let bannerImages = ["url('https://wovenmagazine.com/content/uploads/2019/09/01-CodyCobb_banner.jpg')",
+                    "url('https://wovenmagazine.com/content/uploads/2018/07/04_BeeRog_banner.jpg')",
+                    "url('https://wovenmagazine.com/content/uploads/2018/04/01_-AceJig_cover-banner.jpg')",
+                    "url('https://wovenmagazine.com/content/uploads/2018/07/01_JDR__banner.jpg')"]
+
+
+const changeToNextBanner = () => { 
+    document.getElementById('main-banner').style.backgroundImage = bannerImages[bgCounter];
+    if (bgCounter === 3) {
+        bgCounter = 0;
+    } else {
+        bgCounter++;
+    }
+}
+
+const changeToPrevBanner = () => { 
+    document.getElementById('main-banner').style.backgroundImage = bannerImages[bgCounter];
+    if (bgCounter === 0) {
+        bgCounter = 3;
+    } else {
+        bgCounter--;
+    }
+}
+
+document.getElementById('prev').addEventListener('click', changeToPrevBanner);
+document.getElementById('next').addEventListener('click', changeToNextBanner);
